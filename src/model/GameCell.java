2c36dc9d8f05f4233f518e3c8be0bbff3e757d0d
ClauHC <clauhc90@gmail.com>
2025-03-06 12:40:13 +0100

    Removing an unnecessary parameter and method that had been added to the GameCell class and adding comments II

--- src/model/GameCell.java
+++ src/model/GameCell.java
@@ -4,7 +4,7 @@ public abstract class GameCell {
 
     private boolean isDiscovered;
     protected String content;
-    private boolean isVisible; //  AGREGO. Nuevo atributo para hacer visible la celda
+    private boolean isVisible; //  AGREGO VARIABLE. Nuevo atributo para hacer visible la celda
 
     /**
      * Constructor de la clase GameCell
@@ -12,8 +12,6 @@ public abstract class GameCell {
      */
     public GameCell(String content) {
         this.content = content;
-        this.isDiscovered = false; // AGREGO. Por defecto, las casillas no están descubiertas (por defecto están en 00 y no en ·.)
-        this.isVisible = false; // AGREGO. Por defecto, las casillas no son visibles (por defecto están en 00 y no en 10, 20, 30, --, ++, CC)
     }
 
     /**
@@ -36,7 +34,7 @@ public abstract class GameCell {
      * @return
      */
     public void setVisible() {
-        isVisible = true; // AGREGO. Hace que el contendio de la celda sea visible
+        isVisible = true; // AGREGO MÉTOD. Hace que el contendio de la celda sea visible
     }
 
     /*
@@ -58,3 +56,24 @@ public abstract class GameCell {
         return isDiscovered ? "·." : "00"; // Si no es visible, sigue la lógica normal
     }
 }
+
+/*
+ La clase GameCell es una clase abstracta que representa una casilla del tablero del juego, es la clase de la que
+ parten todas las casillas (celdas) y por tanto la más importante. Esta clase había sido dejada creada previamente
+ como parte del enunciado del ejercicio.
+
+ El funcionamiento original de la clase NO HA SIDO ALTERADO, únicamente se ha añadido una variable booleana isVisible
+ ya que el código del programa según la lógica que se ha seguido lo demandaba, ya que al ser un comportamiento común
+ para todas las clases de celdas que extienden a esta este era el lugar donde debía ponerse.
+
+ isDsicovered se ha usado para indicar si la celda ha sido descubierta (visitada) o no por el ratón.
+
+ Se ha añadido un nuevo métod setVisible() que hace que la casilla sea visible, es decir, que muestre su contenido real.
+
+ Se han agregado líneas al métod toString() para que use isVisble además de usar isDiscover como ya hacía y muestre
+ el contenido real de la celda (10, 20, 30, --, ++, CC, CH) si es visible y si no, muestre "00" o "·." según si está
+ descubierta o no por el ratón.
+
+ Content almacena el contenido real de la celda. Este contenido es diferente para cada tipo de celda que extiende a GameCell.
+ Según este visible o no (isVisible), descubierta o no (isDiscover) es lo que se muestra al usuario en el tablero.
+ */
